f7d1268ec26563759d6a333efd217010fcb63acd
Brian Hulbert <Brian.hulbert@me.com>
2018-11-17 13:05:39 -0500

    Implemented resource types

---
+++ Rock.java
@@ -0,0 +1,6 @@
+import java.awt.*;
+public class Rock extends Resource{
+    public Rock(double x_loc,double y_loc){
+        super(x_loc,y_loc,30,30,"rock",Color.GRAY);
+    }
+}
---
+++ Tree.java
@@ -0,0 +1,6 @@
+import java.awt.*;
+public class Tree extends Resource{
+    public Tree(double x_loc, double y_loc){
+        super(x_loc,y_loc,30,30,"tree",Color.GREEN);
+    }
+}
