b76a701bd2b90d007ba61c52cb4869c9f8276a99
Brian Hulbert <Brian.hulbert@me.com>
2019-04-06 17:17:45 -0400

    added more tech levels

--- TechTree.java
+++ TechTree.java
@@ -8,6 +8,7 @@ public class TechTree{
         techLevelUpRequirments = new ArrayList<String[]>();
         techLevelUpRequirments.add(new String[]{"wood planks:1","stick:1"});
         techLevelUpRequirments.add(new String[]{"stone brick:1"});
+        techLevelUpRequirments.add(new String[]{"stone brick:1","wood planks:1","stick:1"});
         itemsRequired = new ArrayList<TempItem>();
         levelUp();
     }
