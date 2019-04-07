b6494aedde88a0c4577583d16f21cad0ea4b657e
Tristan Lonsway <tblonswa@syr.edu>
2019-04-07 00:06:58 -0400

    Images of trees are now drawn in place of squares. Sizes of resources changes, removed wasteful file loading.

--- Resource.java
+++ Resource.java
@@ -1,5 +1,9 @@
 import javafx.geometry.*;
 import java.awt.*;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 public abstract class Resource implements Clickable {
     private double x;
     private double y;
@@ -12,6 +16,7 @@ public abstract class Resource implements Clickable {
     private double health;
     private Item yield;
     private boolean destroyed;
+    private BufferedImage icon;
     public Resource(double X, double Y, double H, double W, String T, Color C, double MH,String YN, int maxquant, int cat){
         x = X;
         y = Y;
@@ -24,6 +29,11 @@ public abstract class Resource implements Clickable {
         yield = new Item(YN,(int)(Math.random()*maxquant)+1,false,null,null,null,new int[]{0,0,0,0,0},0);
         category=cat;
         destroyed=false;
+        try {
+            icon=ImageIO.read(new File("images/"+type+".png"));
+        } catch (Exception e) {
+            System.out.println("failed to load image for " + type);
+        }
     }
     public double getX(){
         return x;
@@ -67,5 +77,8 @@ public abstract class Resource implements Clickable {
     }
     public boolean isDestroyed() {
         return destroyed;
+    }
+    public BufferedImage getIcon() {
+        return icon;
     }
 }
