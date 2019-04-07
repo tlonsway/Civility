89438817876c6f62b0db8bc4bebc27998c438a46
Tristan Lonsway <tblonswa@syr.edu>
2019-04-07 18:17:59 -0400

    Changed size of pine tree

--- PineTree.java
+++ PineTree.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 public class PineTree extends Resource{
     public PineTree(double x_loc,double y_loc){
-        super(x_loc,y_loc,30,30,"pinetree",new Color(0,120,10),100,"wood",6,0);
+        super(x_loc,y_loc,110,80,"pinetree",new Color(0,120,10),100,"wood",6,0);
     }
     public void click() {
         
